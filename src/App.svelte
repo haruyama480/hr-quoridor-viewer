bf4137e7dfda007545f75be02f6cb21578c9abdf
kazusaku <kazusaku@yahoo-corp.jp>
2021-10-02 13:33:41 +0900

    impl base board

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,133 @@
 <script lang="ts">
-	export let name: string;
-</script>
+	export let game_row_size: number;
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+	game_row_size=5;
+	$: inner_n = game_row_size * 2+1;
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
+	function isMargin(i:number) {
+		return i === 0 || i === inner_n - 1; // first or last
+	}
+	function isPath(i:number) {
+		return i%2 === 0 && !isMargin(i);
+	}
+	function hasCell(i:number) {
+		return !isMargin(i) && !isPath(i);
+	}
+	function isCell(i:number,j :number):any {
+		return hasCell(i) && hasCell(j);
 	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
+	function handleMouseEnter(i:number,j :number):any {
+		return (event) => {
+			console.log(i,j, event);
 		}
 	}
+	function handleClick(i:number,j :number):any {
+		return (event) => {
+			console.log(i,j, event);
+		}
+	}
+</script>
+
+
+<div class="rowContainer">
+	{#each Array(inner_n) as _, i}
+		<div
+			class='columnContainer'
+			class:rowMargin='{isMargin(i)}'
+			class:rowPath='{isPath(i)}'
+			class:row='{hasCell(i)}'
+		>
+			{#each Array(inner_n) as _, j}
+				<div
+					class:columnMargin='{isMargin(j)}'
+					class:columnPath='{isPath(j)}'
+					class:column='{hasCell(j)}'
+					class:cell='{isCell(i,j)}'
+					on:mouseenter={handleMouseEnter(i,j)}
+					on:click={handleClick(i,j)}
+				>
+					{#if isCell(i,j)}
+						<div class='circle'></div>
+					{/if}
+				</div>
+			{/each}
+		</div>
+	{/each}
+</div>
+
+
+<style type="text/css">
+
+/* grid layout */
+.rowContainer {
+	display: flex;
+	flex-direction: column;
+	width: 100%;
+	height: 100%;
+}
+.row{
+	flex: 1 auto;
+}
+.rowPath{
+	flex: 0.2 auto;
+}
+.rowMargin{
+	flex: 0.3 auto;
+}
+
+.columnContainer {
+	display: flex;
+	flex-direction: row;
+	width: 100%;
+}
+.column{
+	flex: 1 auto;
+}
+.cell{
+	display: flex;
+}
+.columnPath {
+	flex: 0.2 auto;
+}
+.columnMargin{
+	flex: 0.3 auto;
+}
+
+
+/* color */
+.cell {
+	background-color: rgb(161,142,128);
+}
+.rowPath, .columnPath, .rowMargin :not(.columnMargin), .columnMargin{
+	background-color: rgb(177,160,146);
+}
+.rowContainer>.columnContainer:first-child>:first-child{
+	border-radius: 100% 0 0 0;
+}
+.rowContainer>.columnContainer:first-child>:last-child{
+	border-radius: 0 100% 0 0;
+}
+.rowContainer>.columnContainer:last-child>:last-child{
+	border-radius: 0 0 100% 0;
+}
+.rowContainer>.columnContainer:last-child>:first-child{
+	border-radius: 0 0 0 100%;
+}
+
+.circle {
+	margin: auto;
+  border-radius: 50%;
+  /* font-size: 50%; */
+	height: 90%;
+	width: 90%;
+  color:red;
+  text-align: center;
+  background: #000;
+	display: none;
+}
+.cell:hover .circle{
+	display:block;
+}
+
 </style>
