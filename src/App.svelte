87154f4a66267624b3a11feec83f54060a152c67
kazusaku <kazusaku@yahoo-corp.jp>
2021-10-03 01:50:36 +0900

    tweak

--- src/App.svelte
+++ src/App.svelte
@@ -14,17 +14,17 @@
   let ghost_vertical_wall: number[][];
   let ghost_horizontal_wall: number[][];
 
-  ghost_pawn = [...Array(game_row_size)].map((i) =>
+  ghost_pawn = [...Array(game_row_size)].map(() =>
     Array(game_row_size).fill(1)
   ); // fill 0 with size(n,n)
   ghost_pawn[0][0] = 1;
   ghost_pawn[1][1] = 1;
-  ghost_vertical_wall = [...Array(game_row_size - 1)].map((i) =>
+  ghost_vertical_wall = [...Array(game_row_size - 1)].map(() =>
     Array(game_row_size - 1).fill(1)
   ); // fill 0 with size(n-1,n-1)
   ghost_vertical_wall[0][0] = 1;
   ghost_vertical_wall[1][1] = 1;
-  ghost_horizontal_wall = [...Array(game_row_size - 1)].map((i) =>
+  ghost_horizontal_wall = [...Array(game_row_size - 1)].map(() =>
     Array(game_row_size - 1).fill(1)
   ); // fill 0 with size(n-1,n-1)
   ghost_horizontal_wall[0][0] = 1;
