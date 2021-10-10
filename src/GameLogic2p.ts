5a7b545ba722a6c6e875634a014ca554a8fa24c1
kazusaku <kazusaku@yahoo-corp.jp>
2021-10-10 18:40:28 +0900

    refactor GameLogic2p

--- src/GameLogic2p.ts
+++ src/GameLogic2p.ts
@@ -1,39 +1,88 @@
-import type { Board, Grid, Position } from "./Layout";
-import { Ghost, Piece } from "./Layout";
+import { updatePawnGhost, validatePawn, validateWall } from "./GameLogicCommon";
+import type { Board, Grid, GridType, Position } from "./Layout";
+import { Ghost, None, Piece } from "./Layout";
 
-export function initState(grid_size: number): {
+export class Game2p {
+  grid_size: number;
   board: Board;
   current_pawn: Position[];
-  goal: Position[][]; // goal[i]: all user_i's goals
-} {
-  const n = grid_size;
-  const pawn: Grid = [...Array(n)].map(() => Array(n).fill(Ghost)); // size(n,n)
-  const center = Math.floor(n / 2);
-  const current_pawn: Position[] = [
-    [0, center],
-    [n - 1, center],
-  ]; // supports only 2-player game
-  pawn[0][center] = Piece(0, false);
-  pawn[n - 1][center] = Piece(1, false);
-  const goal: Position[][] = [
-    [...Array(n)].map((_, i) => [n - 1, i]),
-    [...Array(n)].map((_, i) => [0, i]),
-  ];
+  goal: Position[][];
+  current_player: number;
 
-  const vertical_wall: Grid = [...Array(n - 1)].map(() =>
-    Array(n - 1).fill(Ghost)
-  ); // size(n-1,n-1)
-  const horizontal_wall: Grid = [...Array(n - 1)].map(() =>
-    Array(n - 1).fill(Ghost)
-  ); // size(n-1,n-1)
+  constructor(grid_size: number) {
+    this.grid_size = grid_size;
+    const n = grid_size;
 
-  return {
-    board: {
+    // set board
+    const pawn: Grid = [...Array(n)].map(() => Array(n).fill(Ghost)); // size(n,n)
+    const center = Math.floor(n / 2);
+    pawn[0][center] = Piece(0, false);
+    pawn[n - 1][center] = Piece(1, false);
+    const vertical_wall: Grid = [...Array(n - 1)].map(() =>
+      Array(n - 1).fill(Ghost)
+    ); // size(n-1,n-1)
+    const horizontal_wall: Grid = [...Array(n - 1)].map(() =>
+      Array(n - 1).fill(Ghost)
+    ); // size(n-1,n-1)
+    this.board = {
       pawn,
       vertical_wall,
       horizontal_wall,
-    },
-    current_pawn,
-    goal,
-  };
+    };
+
+    // set current_pawn, goal
+    this.current_pawn = [
+      [0, center],
+      [n - 1, center],
+    ]; // supports only 2-player game
+    this.goal = [
+      [...Array(n)].map((_, i) => [n - 1, i]),
+      [...Array(n)].map((_, i) => [0, i]),
+    ];
+
+    this.current_player = -1;
+    this.nextTurn();
+  }
+
+  public nextTurn(): void {
+    this.current_player = (this.current_player + 1) % 2;
+    updatePawnGhost(this.current_pawn[this.current_player], this.board);
+  }
+
+  public handleTurn(gridType: GridType, next: Position): boolean {
+    const current: Position = this.current_pawn[this.current_player];
+    const [y, x] = next;
+    if (gridType.kind === "pawn") {
+      if (!validatePawn(current, next, this.board)) {
+        return false;
+      }
+      const pawn_: Grid = JSON.parse(JSON.stringify(this.board.pawn)); // deep copy
+
+      const [prey, prex] = current;
+      pawn_[prey][prex] = None; // from
+      pawn_[y][x] = Piece(this.current_player, false); // to
+      this.board.pawn = pawn_;
+      this.current_pawn[this.current_player] = [y, x];
+      return true;
+    }
+
+    const board_ = JSON.parse(JSON.stringify(this.board)); // deep copy
+    if (gridType.kind === "vwall") {
+      if (y === this.grid_size - 1) return false;
+      if (this.board.vertical_wall[y][x].kind === "piece") {
+        return false;
+      }
+      board_.vertical_wall[y][x] = Piece(this.current_player, false);
+    } else if (gridType.kind === "hwall") {
+      if (x === this.grid_size - 1) return false;
+      if (this.board.horizontal_wall[y][x].kind === "piece") {
+        return false;
+      }
+      board_.horizontal_wall[y][x] = Piece(this.current_player, false);
+    }
+    if (validateWall(this.current_pawn, this.goal, board_)) {
+      this.board = board_;
+      return true;
+    }
+  }
 }
--- src/sample/SimpleBoard.svelte
+++ src/sample/SimpleBoard.svelte
@@ -1,69 +1,26 @@
 <script lang="ts">
   import BoardView from "../BoardView.svelte";
-  import { initState } from "../GameLogic2p";
-  import {
-    updatePawnGhost,
-    validatePawn,
-    validateWall,
-  } from "../GameLogicCommon";
-  import type { Grid, GridType, Position } from "../Layout";
-  import { None, Piece } from "../Layout";
+  import { Game2p } from "../GameLogic2p";
+  import type { GridType, Position } from "../Layout";
 
   export let board_size = "500px";
   export let grid_size = 9;
 
-  const game_player_size = 2;
-
-  let current_player_id = -1; // 0-index
-  let { board, current_pawn, goal } = initState(grid_size);
-  function nextTurn() {
-    current_player_id = (current_player_id + 1) % game_player_size;
-    updatePawnGhost(current_pawn[current_player_id], board);
-  }
-
-  nextTurn();
+  let game = new Game2p(grid_size);
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   function clickCell(event: any): void {
     const gridType: GridType = event.detail.gridType;
     const next: Position = event.detail.nextPosition;
-    const current: Position = current_pawn[current_player_id];
-    let [y, x] = next;
-    if (gridType.kind === "pawn") {
-      if (!validatePawn(current, next, board)) {
-        return;
-      }
-      let pawn_: Grid = JSON.parse(JSON.stringify(board.pawn)); // deep copy
-
-      const [prey, prex] = current;
-      pawn_[prey][prex] = None; // from
-      pawn_[y][x] = Piece(current_player_id, false); // to
-      board.pawn = pawn_;
-      current_pawn[current_player_id] = [y, x];
-      nextTurn();
-      return;
-    }
-
-    let board_ = JSON.parse(JSON.stringify(board)); // deep copy
-    if (gridType.kind === "vwall") {
-      if (y === grid_size - 1) return;
-      if (board.vertical_wall[y][x].kind === "piece") {
-        return;
-      }
-      board_.vertical_wall[y][x] = Piece(current_player_id, false);
-    } else if (gridType.kind === "hwall") {
-      if (x === grid_size - 1) return;
-      if (board.horizontal_wall[y][x].kind === "piece") {
-        return;
-      }
-      board_.horizontal_wall[y][x] = Piece(current_player_id, false);
-    }
-    if (validateWall(current_pawn, goal, board_)) {
-      board = board_;
-      nextTurn();
-      return;
+    if (game.handleTurn(gridType, next)) {
+      game.nextTurn();
+      game = game; // trigger render
     }
   }
+
+  $: grid_size = game.grid_size;
+  $: current_player_id = game.current_player;
+  $: board = game.board;
 </script>
 
 <div style="--board-area: {board_size}">
